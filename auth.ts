acb279800b183dcc548b941a90150cef0f2c3030
Destu Cikal <destucr@gmail.com>
2025-12-27 01:26:12 +0700

    fix: auth build

--- auth.ts
+++ auth.ts
@@ -2,13 +2,9 @@ import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+    basePath: "/api/auth",
     trustHost: true,
-    providers: [
-        GitHub({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-        })
-    ],
+    providers: [GitHub],
     callbacks: {
         async signIn({ user }) {
             // ONLY allow your specific email to access the admin gateway
