957e3798b32fbe161b8f539abf11204464a13090
Destu Cikal <destucr@gmail.com>
2025-12-26 21:18:29 +0700

    fix: dependency resolution

--- next.config.ts
+++ next.config.ts
@@ -14,10 +14,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  // Fixes the Cross-origin warning for local development
-  experimental: {
-    allowedDevOrigins: ["http://localhost:3000", "http://127.0.0.1:3000"]
-  }
 };
 
 export default nextConfig;
