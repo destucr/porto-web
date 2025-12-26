f3b33ff81d8079adad91e214215a55bc4fc98907
Destu Cikal <destucr@gmail.com>
2025-12-27 03:31:36 +0700

    fix: explicitly pass keystatic credentials to route handler for production

--- app/api/keystatic/[...params]/route.ts
+++ app/api/keystatic/[...params]/route.ts
@@ -3,6 +3,9 @@ import config from '../../../../keystatic.config';
 
 export const { GET, POST } = makeRouteHandler({
   config,
+  clientId: process.env.NEXT_PUBLIC_KEYSTATIC_GITHUB_CLIENT_ID || process.env.KEYSTATIC_GITHUB_CLIENT_ID,
+  clientSecret: process.env.KEYSTATIC_GITHUB_CLIENT_SECRET,
+  secret: process.env.KEYSTATIC_SECRET,
 });
 
 export const dynamic = 'force-dynamic';
