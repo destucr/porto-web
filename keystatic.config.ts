123757fa998d37d6bd246a4600ca5617b5eacb80
Destu Cikal <destucr@gmail.com>
2025-12-27 12:09:59 +0700

    fix: remove unsupported clientId from keystatic config storage

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -5,7 +5,6 @@ export default config({
     ? {
         kind: 'github',
         repo: { owner: 'destucr', name: 'porto-web' },
-        clientId: process.env.NEXT_PUBLIC_KEYSTATIC_GITHUB_CLIENT_ID,
       }
     : {
         kind: 'local',
