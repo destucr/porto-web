ac6b49254cf5cee6c09e94317b7dade38d9b95cb
Destu Cikal <destucr@gmail.com>
2025-12-27 12:23:51 +0700

    fix: inject keystatic client id at runtime and improve route handler

--- app/keystatic/[[...params]]/page.tsx
+++ app/keystatic/[[...params]]/page.tsx
@@ -1,5 +1,15 @@
 import KeystaticApp from "../keystatic";
 
 export default function Page() {
-  return <KeystaticApp />;
+  return (
+    <>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.process = window.process || { env: {} }; 
+                   window.process.env.NEXT_PUBLIC_KEYSTATIC_GITHUB_CLIENT_ID = "${process.env.NEXT_PUBLIC_KEYSTATIC_GITHUB_CLIENT_ID || process.env.KEYSTATIC_GITHUB_CLIENT_ID || ''}";`,
+        }}
+      />
+      <KeystaticApp />
+    </>
+  );
 }
