40186a070c1f53c595a9b8b4724fcd347a57f38d
Destu Cikal <destucr@gmail.com>
2025-12-27 12:24:43 +0700

    fix: strictly separate keystatic and auth credentials

--- app/api/keystatic/[...params]/route.ts
+++ app/api/keystatic/[...params]/route.ts
@@ -6,8 +6,8 @@ export const dynamic = 'force-dynamic';
 const handler = (process.env.KEYSTATIC_SECRET || process.env.NODE_ENV === 'development')
   ? makeRouteHandler({
       config,
-      clientId: process.env.NEXT_PUBLIC_KEYSTATIC_GITHUB_CLIENT_ID || process.env.KEYSTATIC_GITHUB_CLIENT_ID || process.env.AUTH_GITHUB_ID,
-      clientSecret: process.env.KEYSTATIC_GITHUB_CLIENT_SECRET || process.env.AUTH_GITHUB_SECRET,
+      clientId: process.env.KEYSTATIC_GITHUB_CLIENT_ID || process.env.NEXT_PUBLIC_KEYSTATIC_GITHUB_CLIENT_ID,
+      clientSecret: process.env.KEYSTATIC_GITHUB_CLIENT_SECRET,
       secret: process.env.KEYSTATIC_SECRET,
     })
   : { GET: () => new Response('Not found', { status: 404 }), POST: () => new Response('Not found', { status: 404 }) };
