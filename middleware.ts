cfa1a393db18748e50372fd2be7f2bf129715d9b
Destu Cikal <destucr@gmail.com>
2026-02-09 18:17:03 +0700

    fix: allow internal embedding for live previews

--- middleware.ts
+++ middleware.ts
@@ -17,7 +17,7 @@ export function middleware(request: NextRequest) {
   // Protocol & Security Headers
   response.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload')
   response.headers.set('X-Content-Type-Options', 'nosniff')
-  response.headers.set('X-Frame-Options', 'DENY')
+  response.headers.set('X-Frame-Options', 'SAMEORIGIN')
   response.headers.set('X-XSS-Protection', '1; mode=block')
   response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin')
 
